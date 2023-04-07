3cd595ea09cfd214838f9d8514c5229856636acd
Frank Matranga <thefrankmatranga@gmail.com>
2023-04-06 21:34:50 -0400

    Update share urls

--- src/podcast.ts
+++ src/podcast.ts
@@ -6,7 +6,7 @@ export const PODCAST_TAGLINE =
   "A very serious research podcast about weird, local stories.";
 export const PODCAST_EMAIL = "wednesdayatninepm@gmail.com";
 export const PODCAST_DESCRIPTION =
-  "Join Frank, Nick, and Aaron as they research and present local strange stories that will make you question the validity of their STEM degrees.";
+  "Join Frank, Nick, and Aaron as they research and present local, strange stories that will make you question the validity of their STEM degrees.";
 export const EPISODE_PREFACE =
   "This Wednesday night at 9pm at midnight at 3am in a cemetery... ";
 export const PODCAST_HOSTS: PEOPLE_NAME[] = ["Aaron", "Frank", "Nick"];
@@ -16,6 +16,13 @@ export type Person = {
   imgUrl?: string;
   url?: string;
 };
+
+export const SHARE_URLS = {
+  spotify: "https://open.spotify.com/show/15dzhmwVaXot0JDilN0AR7",
+  google:
+    "https://podcasts.google.com/feed/aHR0cHM6Ly93ZWRuZXNkYXlhdG5pbmVwbS5jb20vZmVlZC5yc3M",
+  apple: "https://podcasts.apple.com/us/podcast/wednesday-at-9pm/id1680373413",
+} as const;
 
 export const PEOPLE_NAMES = ["Frank", "Nick", "Aaron"] as const;
 export type PEOPLE_NAME = (typeof PEOPLE_NAMES)[number];
