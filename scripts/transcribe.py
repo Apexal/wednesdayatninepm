1b0468d4d2224f4cd8e50ff916ba28400fd56e88
Frank Matranga <thefrankmatranga@gmail.com>
2023-04-23 20:53:30 -0400

    Testing

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -1,33 +1,58 @@
 import json
-import timeit
+from typing import Dict, List, Union
 import whisper
-import time
-
-model = whisper.load_model("small.en")
-episode_slug = input("episode slug: ")
 
 
-def transcribe(slug: str):
+def transcribe():
+    """
+    Finds the audio file, transcribes it to JSON with timestamps for each word, and constructs
+    JSON, SRT, VTT, and HTML transcripts in the format specified by the Podcast 2.0 feed.
+    """
+    model = whisper.load_model("small.en")
+    episode_slug = input("episode slug: ")
+
     result = model.transcribe(episode_slug + ".m4a", word_timestamps=True)
 
+    with open(episode_slug + "_transcribed.json", "w") as f:
+        f.write(json.dumps(result))
+
+    export_as_json(result)
+
+
+def export_as_json(result: Dict[str, Union[str, List]], filename: str):
     segments = []
+    buffer = ""
+    start = result["segments"][0]["start"]
+    end = start
     for segment in result["segments"]:
         for word in segment["words"]:
-            segments.append(
-                {
-                    "startTime": word["start"],
-                    "endTime": word["end"],
-                    "body": word["word"].strip(),
-                }
-            )
+            if len((buffer + word["word"]).strip()) > 32:
+                segments.append(
+                    {"startTime": start, "endTime": end, "body": buffer.strip()}
+                )
+
+                # Reset for next segment
+                buffer = word["word"]
+                start = word["start"]
+            else:
+                buffer += word["word"]
+                end = word["end"]
+
+    if len(buffer.strip()) > 0:
+        print(start, end, buffer)
+        segments.append({"startTime": start, "endTime": end, "body": buffer.strip()})
 
     # see https://github.com/Podcastindex-org/podcast-namespace/blob/main/transcripts/transcripts.md
     output = {"version": "1.0.0", "segments": segments}
 
-    with open(episode_slug + ".json", "w") as f:
-        f.write(json.dumps(output))
+    with open(filename + ".json", "w") as f:
+        f.write(json.dumps(output, indent=2))
 
     return output
 
 
-transcribe(episode_slug)
+# transcribe()
+
+if __name__ == "__main__":
+    with open("Recording_transcribed.json") as f:
+        export_as_json(json.loads(f.read()), "testing")
