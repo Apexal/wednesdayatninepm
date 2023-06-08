0ea5ad6913745dc60626ffd50ab35e5e83bb2ab2
Frank Matranga <thefrankmatranga@gmail.com>
2023-06-08 00:12:35 -0400

    Remove log message

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -2,8 +2,6 @@ import { defineMarkdocConfig } from "@astrojs/markdoc/config";
 
 import AmazonBookPreview from "./src/components/AmazonBookPreview.astro";
 
-console.log("HERE EREREAR ");
-
 export default defineMarkdocConfig({
   tags: {
     book: {
